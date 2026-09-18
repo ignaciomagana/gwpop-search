d0a37e3511616baf4bdc8fd2653becd055cc3a47
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-18 01:08:53 -0400

    Expose scout review and independent comparison APIs

--- src/gwpop_search/scouts/__init__.py
+++ src/gwpop_search/scouts/__init__.py
@@ -1,5 +1,9 @@
 """Flexible residual scouts that propose, but never auto-promote, structure."""
 
+from .comparison import (
+    compare_scout_descendant_evidence,
+    scout_comparison_seed_root,
+)
 from .campaign import (
     assess_structured_scout_campaign,
     build_structured_scout_campaign_plan,
@@ -28,6 +32,12 @@ from .synthetic import (
     StructuredScoutInjection,
     generate_structured_scout_dataset,
 )
+from .review import (
+    ReviewedScoutProposal,
+    review_scout_proposal,
+    select_validated_scout_proposal,
+    write_scout_review,
+)
 from .summary import (
     ConditionalMomentSummaryConfig,
     summarize_conditional_hsgp,
@@ -51,6 +61,7 @@ from .proposals import (
 
 __all__ = [
     "ConditionalHSGPConfig",
+    "ReviewedScoutProposal",
     "ScoutCampaignConfig",
     "StructuredScoutInjection",
     "ConditionalHSGPResidualModel",
@@ -62,6 +73,7 @@ __all__ = [
     "ScoutNumericalCriteria",
     "StructureProposal",
     "assess_scout_numerics",
+    "compare_scout_descendant_evidence",
     "assess_structured_scout_campaign",
     "build_structured_scout_campaign_plan",
     "default_scout_campaign_config",
@@ -75,11 +87,15 @@ __all__ = [
     "proposal_from_summary",
     "generate_structured_scout_dataset",
     "reachable_mutation_ids",
+    "review_scout_proposal",
     "run_conditional_hsgp_scout",
     "run_structured_scout_campaign",
     "save_scout_campaign_config",
+    "scout_comparison_seed_root",
+    "select_validated_scout_proposal",
     "squared_exponential_spectral_weights",
     "structured_scout_seed",
     "summarize_conditional_hsgp",
     "weighted_linear_dependence",
+    "write_scout_review",
 ]
