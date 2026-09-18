9e5bddc8ecc434e00f63a98789051e170c95d6a1
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-17 22:37:35 -0400

    Expose declarative compiled population models

--- src/gwpop_search/models/__init__.py
+++ src/gwpop_search/models/__init__.py
@@ -3,24 +3,35 @@
 from .baseline import DEFAULT_BASELINE_HYPERPARAMETERS, GwcatChiEffBBHModel
 from .components import (
     chi_eff_logpdf,
+    chi_eff_mixture_logpdf,
     mass_ratio_logpdf,
+    mass_ratio_truncated_normal_logpdf,
     powerlaw_logpdf,
     primary_mass_broken_powerlaw_logpdf,
     primary_mass_powerlaw_peak_logpdf,
+    primary_mass_powerlaw_two_peak_logpdf,
+    redshift_madau_dickinson_logpdf,
     redshift_rate_logpdf,
     truncated_normal_logpdf,
 )
 from .cosmology import FlatLambdaCDM
+from .declarative import DeclarativeGwcatChiEffModel, compile_model_spec
 
 __all__ = [
     "DEFAULT_BASELINE_HYPERPARAMETERS",
+    "DeclarativeGwcatChiEffModel",
     "FlatLambdaCDM",
     "GwcatChiEffBBHModel",
     "chi_eff_logpdf",
+    "chi_eff_mixture_logpdf",
+    "compile_model_spec",
     "mass_ratio_logpdf",
+    "mass_ratio_truncated_normal_logpdf",
     "powerlaw_logpdf",
     "primary_mass_broken_powerlaw_logpdf",
     "primary_mass_powerlaw_peak_logpdf",
+    "primary_mass_powerlaw_two_peak_logpdf",
+    "redshift_madau_dickinson_logpdf",
     "redshift_rate_logpdf",
     "truncated_normal_logpdf",
 ]
