8f66ab57fad7ba1f986bfac6bdd0a1df5a444a7b
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-17 23:40:07 -0400

    Expose normalized HSGP scout inference API

--- src/gwpop_search/scouts/__init__.py
+++ src/gwpop_search/scouts/__init__.py
@@ -1,5 +1,20 @@
 """Flexible residual scouts that propose, but never auto-promote, structure."""
 
+from .conditional import (
+    ConditionalHSGPConfig,
+    ConditionalHSGPResidualModel,
+    coefficient_priors,
+)
+from .inference import (
+    ConditionalScoutRunConfig,
+    ScoutNumericalCriteria,
+    assess_scout_numerics,
+    run_conditional_hsgp_scout,
+)
+from .summary import (
+    ConditionalMomentSummaryConfig,
+    summarize_conditional_hsgp,
+)
 from .hsgp import (
     HSGPAxis,
     HSGPDesign,
@@ -16,14 +31,23 @@ from .proposals import (
 )
 
 __all__ = [
+    "ConditionalHSGPConfig",
+    "ConditionalHSGPResidualModel",
+    "ConditionalMomentSummaryConfig",
+    "ConditionalScoutRunConfig",
     "HSGPAxis",
     "HSGPDesign",
     "ResidualDependenceSummary",
+    "ScoutNumericalCriteria",
     "StructureProposal",
+    "assess_scout_numerics",
     "basis_matrix",
+    "coefficient_priors",
     "laplacian_frequencies",
     "mutation_for_proposal",
     "proposal_from_summary",
+    "run_conditional_hsgp_scout",
     "squared_exponential_spectral_weights",
+    "summarize_conditional_hsgp",
     "weighted_linear_dependence",
 ]
