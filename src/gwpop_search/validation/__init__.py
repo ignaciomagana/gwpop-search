00c5dc3a5e598def9b2a99c7ad19672cfc40b427
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-18 00:53:55 -0400

    Expose event-drop stress validation API

--- src/gwpop_search/validation/__init__.py
+++ src/gwpop_search/validation/__init__.py
@@ -12,4 +12,25 @@ __all__ = [
     "deterministic_event_folds",
     "heldout_detected_log_predictive",
     "validate_hyperposterior_samples",
+    "EventDropScenario",
+    "EventStressConfig",
+    "build_event_stress_plan",
+    "compare_edge_bayes_factors",
+    "edge_log_bayes_factors",
+    "leave_one_out_scenarios",
+    "run_event_drop_stress_suite",
+    "stress_dataset_identity",
+    "stress_seed",
 ]
+
+from .stress import (
+    EventDropScenario,
+    EventStressConfig,
+    build_event_stress_plan,
+    compare_edge_bayes_factors,
+    edge_log_bayes_factors,
+    leave_one_out_scenarios,
+    run_event_drop_stress_suite,
+    stress_dataset_identity,
+    stress_seed,
+)
