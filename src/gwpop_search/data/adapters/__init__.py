d452744622793fded86edcc68779444275e6ab83
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-17 21:55:18 -0400

    Export gwcat-v2 density basis helper

--- src/gwpop_search/data/adapters/__init__.py
+++ src/gwpop_search/data/adapters/__init__.py
@@ -1,10 +1,12 @@
 """Adapters from external validated data products into canonical containers."""
 
+from .gwcat_v2 import basis_for_spin as gwcat_v2_basis_for_spin
 from .gwcat_v2 import load_pair as load_gwcat_v2_pair
 from .gwcat_v2 import load_pe as load_gwcat_v2_pe
 from .gwcat_v2 import load_selection as load_gwcat_v2_selection
 
 __all__ = [
+    "gwcat_v2_basis_for_spin",
     "load_gwcat_v2_pair",
     "load_gwcat_v2_pe",
     "load_gwcat_v2_selection",
